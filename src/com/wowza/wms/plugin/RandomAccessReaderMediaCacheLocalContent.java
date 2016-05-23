f75852bff417ee616e8c51338672222c5ff31dbf
Roger Littin <roger@wowza.com>
2016-05-23 14:14:29 +1200

    Updated package path

--- src/com/wowza/wms/plugin/RandomAccessReaderMediaCacheLocalContent.java
+++ src/com/wowza/wms/plugin/RandomAccessReaderMediaCacheLocalContent.java
@@ -1,8 +1,8 @@
-/**
+/*
  * This code and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC. All rights reserved.
  * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
  */
-package com.wowza.wms.plugin.mediacachelocalcontent;
+package com.wowza.wms.plugin;
 
 import java.io.File;
 import java.io.IOException;
